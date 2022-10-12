646edb48550501578ab423ded927db283f94567b
asror797 <asror797shahobiddinov@gmail.com>
2022-10-12 23:53:11 +0500

    ekfknffnef

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '5792347895:AAFr_P4cgjFRYpnZaydqCOFMrjVT713G27E';
-
+let fetch = import('node-fetch')
 const bot = new TelegramBot(token, {polling: true});
 
 
@@ -100,15 +100,62 @@ bot.on('message', (msg) => {
       })
    }
 
+
+   const InlineButton = {
+      reply_markup: {
+         inline_keyboard: [
+            [
+               {
+                  text:"Inline",
+                  callback_data:"callback"
+               }
+            ]
+         ]
+      }
+   }
+
  
 
    if(msg.text === '/start' || msg.text === "⬅️ Orqaga") {
-      bot.sendMessage(chatId,"Asosiy menu",MainMenu)
+
+      fetch('http://localhost:9000/new',{
+         method:"POST",
+         headers:{
+            "Content-Type":"application/json"
+         },
+         body: JSON.stringify({
+            fullname:msg.chat.first_name,
+            telegramid:chatId,
+            phonenumber:"",
+            location:""
+         })
+      })
+         .then(res => res.json())
+         .then(data => {
+            bot.sendMessage(chatId,"Asosiy menu",MainMenu)
+            console.log(data)
+         })
+         .catch(err => {
+            console.log(err)
+            bot.sendMessage(chatId,"Error")
+         })
    }else if (msg.text === '🛍 Buyurtma berish') {
       bot.sendMessage(chatId,"Buyurtmangizni davom ettiring",Products)
    } 
 
+   if(msg.text === 'inline') {
+      bot.sendMessage(chatId,"Savat",InlineButton)
+   }
 
+
+});
+
+
+bot.on("callback_query", (callbackQuery) => {
+   const msg = callbackQuery.message;
+   console.log(callbackQuery)
+   bot.answerCallbackQuery(callbackQuery.id)
+       .then(() => bot.sendMessage(msg.chat.id, "You clicked!"));
 });
 
 
